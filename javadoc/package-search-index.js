ffb9f240010d84505e42ab2539ed8053d0c10b64
Alex Kelvin Hong <35549085+deusalexmachina@users.noreply.github.com>
2024-10-15 15:34:04 -0400

    update javadoc for OA65272
    added documentation for insertNext APIs

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.ibm.eznosql"},{"l":"com.ibm.eznosql.exception"},{"l":"com.ibm.eznosql.options"},{"l":"com.ibm.eznosql.result"}]
