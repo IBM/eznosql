11009a9c3ea7f7946b1e37fc0e12bae22b948042
Alex Kelvin Hong <35549085+deusalexmachina@users.noreply.github.com>
2024-10-22 17:09:22 -0400

    Update conf.py

--- docs/pythondoc/docs/source/conf.py
+++ docs/pythondoc/docs/source/conf.py
@@ -49,4 +49,5 @@ intersphinx_mapping = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_baseurl = 'https://ibm.github.io/eznosql/pythondoc/'
 html_static_path = ['_static']
